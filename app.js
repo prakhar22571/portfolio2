3bbfc11b17972309930a48784e43cc3711604a80
prakhar22571 <prakhar.raicic@gmail.com>
2023-01-27 18:17:41 +0530

    sections & icons

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ function PageTransitions(){
             this.className += 'active-btn';
         })
     }
+
+    //Sections Active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from other buttons
+            secBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
